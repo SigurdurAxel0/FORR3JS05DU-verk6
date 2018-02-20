edc105ecd481b20df74ac70c00b84320b6e042ed
SigurdurAxel0 <sigurdurar263@nemi.tskoli.is>
2018-02-20 20:59:43 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var startItem = document.getElementsByTagName('div')[0];
-startItem.setAttribute('class','active'); 
-
-
-var removeEl = document.getElementsByTagName('div')[2];
-var containerEl = document.getElementsByTagName('main')[0];
-containerEl.removeChild(removeEl);
